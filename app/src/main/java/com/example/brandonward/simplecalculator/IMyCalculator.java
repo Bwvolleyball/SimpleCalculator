972172ae0eecab651acc0b1833ab5293a9698e4a
Brandon Ward <ward486@regis.edu>
2014-07-03 23:55:50 -0600

    Fixed java files back to conventional names.

---
+++ app/src/main/java/com/example/brandonward/simplecalculator/IMyCalculator.java
@@ -0,0 +1,10 @@
+package com.example.brandonward.simplecalculator;
+
+import android.os.Bundle;
+
+/**
+ * Created by BrandonWard on 7/3/2014.
+ */
+public interface IMyCalculator {
+    void onResume(Bundle savedInstanceState);
+}
